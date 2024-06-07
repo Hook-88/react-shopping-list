93adeff97e3b76dd31e17ed624d452c0070ac548
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-06-06 20:02:43 -0500

    add basic styles

--- src/pages/ShoppingListPage.jsx
+++ src/pages/ShoppingListPage.jsx
@@ -1,6 +1,7 @@
 import { FaPlus } from "react-icons/fa6"
 
 import Header from "../components/Header"
+import { IoClose } from "react-icons/io5"
 
 export default function ShoppingListPage() {
     
@@ -12,14 +13,18 @@ export default function ShoppingListPage() {
                     <FaPlus />
                 </button>
             </Header>
-            <main className="px-4 flex flex-col mt-2">
-                <form className="grid grid-cols-6">
-                    <input type="text" placeholder="name" className="col-span-6 text-center"/>
-                    <select className="col-span-2">
+            <main className="px-4 flex flex-col">
+                <form className="grid grid-cols-6 p-4 gap-2 border border-white/35 rounded-lg">
+                    <input type="text" placeholder="name" className="col-span-6 text-center bg-white/10 rounded-lg py-2 mb-2"/>
+                    <select className="col-span-3 bg-white/10 rounded-lg p-2">
                         <option value="general">General</option>
                     </select>
-                    <button type="button" className="col-start-5">Close</button>
-                    <button type="submit">Add</button>
+                    <button type="button" className="col-start-5 bg-red-900 rounded-lg text-2xl flex items-center justify-center">
+                        <IoClose />
+                    </button>
+                    <button type="submit" className="bg-green-900 rounded-lg">Add
+                        
+                    </button>
                 </form>
             </main>
         </>
