237033766837f3877ec34d3f2f4f93278a8abe8d
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-06-14 11:46:00 -0500

    fresh start shoppinglist page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
-import ShoppingListPage from "./pages/ShoppingListPage"
-
 
 export default function App() {
     
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<ShoppingListPage />}/>
+                <Route path="/" element={<h1>hello</h1>}/>
             </Routes>
         </BrowserRouter>
     )
--- src/components/Menu/MenuItem.jsx
+++
@@ -1,8 +0,0 @@
-export default function MenuItem({children, onClick}) {
-    
-    return (
-        <li className="text-nowrap py-2 px-4 border-b border-white/30 last:border-none" onClick={onClick}>
-            {children}
-        </li>
-    )
-}
