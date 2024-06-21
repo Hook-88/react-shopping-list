c505e3ffe1af6819041d4220d9ae0ba304fde936
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-06-20 19:30:53 -0500

    add logic to get data form firebase

---
+++ src/pages/ShoppingList/ShoppingList.jsx
@@ -0,0 +1,32 @@
+import List from "../../components/List/List"
+import AddButton from "../../components/Buttons/AddButton"
+import SubtractButton from "../../components/Buttons/SubtractButton"
+
+export default function ShoppingList({listArr}) {
+    
+    return (
+        <List>
+            {
+                listArr.map(item => (
+                    <List.Item key={item.id}>
+                        {item.name}
+                        {
+                            item.quantity > 1 &&
+                            <>
+                                &nbsp;
+                                {`(${item.quantity}x)`}
+                            </>
+                        }
+                        <div className="ml-auto flex gap-2">
+                            {
+                                item.quantity > 1 && 
+                                <SubtractButton />
+                            }
+                            <AddButton />
+                        </div>
+                    </List.Item>
+                ))
+            }
+        </List>
+    )
+}
--- src/pages/ShoppingList/ShoppingListPage.jsx
+++ src/pages/ShoppingList/ShoppingListPage.jsx
@@ -1,77 +1,39 @@
-import { useEffect, useRef, useState } from "react"
 import IconMore from "../../components/Icons/IconMore"
 import { addDoc, collection, onSnapshot } from "firebase/firestore"
 import { db } from "../../firebase/firebase"
+import { useEffect, useState } from "react"
 import List from "../../components/List/List"
-import ListShopping from "./ListShopping"
-import Card from "../../components/Card"
-import Form from "../../components/Form"
-import Button from "../../components/Buttons/Button"
-import AddItemForm from "./AddItemForm"
-import { useAtom, useAtomValue } from "jotai"
-import { addNewItemAtom, hideCheckedItemsAtom } from "../../store/store"
-import HeaderMenu from "./HeaderMenu"
-import ListShoppingFiltered from "./ListShoppingFiltered"
+import ShoppingList from "./ShoppingList"
+
 
 
 export default function ShoppingListPage() {
-    const [shoppingLists, setShoppingLists] = useState([])
-    const [addNewItemOn, setAddNewItemOn] = useAtom(addNewItemAtom)
-    // const [hideChecked, setHideChecked] = useState(false)
-    const hideChecked = useAtomValue(hideCheckedItemsAtom)
+    const [shoppingList, setShoppingList] = useState([])
 
     useEffect(() => {
         const unsub = onSnapshot(collection(db, "shoppingList"), snapshot => {
-            //sync with local state
-            const newArr = snapshot.docs.map(doc => (
-                {
-                    ...doc.data(),
-                    id: doc.id
-                }
-            ))
+            const newArr = snapshot.docs.map(doc => ({
+                ...doc.data(),
+                id: doc.id
+            }))
 
-            setShoppingLists(newArr)
-
+            setShoppingList(newArr)
         })
 
         return unsub
     }, [])
-
-    async function addItemToList(data) {
-
-        const itemObj = {
-            name: data.itemName.trim().toLowerCase(),
-            quantity: 1,
-            selected: false
-        }
-
-        await addDoc(collection(db, `shoppingList/${data.listId}/items`), itemObj)
-        
-    }
     
     return (
         <>
         <header className="py-2 px-4 grid grid-cols-9 font-bold text-lg fixed inset-x-0 top-0 bg-black/80">
             <h1 className="col-start-2 col-span-7 text-center">Shopping</h1>
-            <HeaderMenu />
+            <IconMore />
         </header>
 
         <main className="px-4 mt-12 flex flex-col gap-4">
             {
-                shoppingLists.map(list => {
-
-                    if (hideChecked) {
-
-                        return <ListShoppingFiltered key={list.id} listObj={list} />
-                    }
-
-                    return (
-                        <ListShopping key={list.id} listObj={list} />
-                    )
-                })
-            }
-            {
-                addNewItemOn && <AddItemForm onSubmit={addItemToList}/>
+                shoppingList.length > 1 ?
+                <ShoppingList listArr={shoppingList} /> : null
             }
         </main>
         </>
