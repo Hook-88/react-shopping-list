771237ecd6751c7738a613702cf082b05d6d4f4b
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-06-23 11:32:30 -0500

    add basic select all button

--- src/pages/Recipe/IngredientsListDefault.jsx
+++ src/pages/Recipe/IngredientsListDefault.jsx
@@ -2,17 +2,29 @@ import { useEffect, useState } from "react"
 import List from "../../components/List/List"
 import { useStore } from "../../store/store"
 import Ingredient from "./Ingredient"
+import IconCheck from "../../components/Icons/IconCheck"
 
 export default function IngredientsListDefault() {
     const ingredients = useStore(state => state.ingredients)
 
     return (        
-        <List>
-            {
-                ingredients.map(ingredient => (
-                    <Ingredient key={ingredient.id} ingredientId={ingredient.id}/> 
-                ))
-            }
-        </List>
+        <div className="flex flex-col gap-1">
+            <List.Header>
+                <List.Progress totalLength={ingredients.length} currentLength={ingredients.length}/>
+                <button className="text-sm col-span-2 col-start-5 justify-end items-center pr-4 flex gap-1">
+                    Select all
+                    <IconCheck />
+                </button>
+            </List.Header>
+
+            <List>
+                {
+                    ingredients.map(ingredient => (
+                        <Ingredient key={ingredient.id} ingredientId={ingredient.id}/> 
+                    ))
+                }
+            </List>
+
+        </div> 
     )
 }
