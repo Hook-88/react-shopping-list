38fbe10341c641f41517d73f26fcc3265b523d0e
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-06-25 15:00:37 -0500

    add list header

--- src/pages/ShoppingList/ShoppingListPage.jsx
+++ src/pages/ShoppingList/ShoppingListPage.jsx
@@ -14,33 +14,41 @@ export default function ShoppingListPage() {
                 <PageHeader.Title className="col-start-3 col-span-5">Shopping List</PageHeader.Title>
             </PageHeader>
             <main className="mt-12 px-4">
-                <ul className="space-y-2">
-                    {
-                        shoppingList.map(
-                            item => (
-                                <li 
-                                    key={item.id}
-                                >   
-                                    <Card className="flex items-center justify-between">
-                                        {item.name}
-                                        &nbsp;
-                                        {   
-                                            item.quantity > 1 &&
-                                            `(${item.quantity}x)`
-                                        }
-                                        <div className="flex gap-2">
-                                            {
-                                                item.quantity > 1 && <ButtonSubtract />
+                <div>
+                    <div className="px-4 mb-1 flex">
+                        <small>(2/2)</small>
+                        <button className="text-sm ml-auto">
+                            hide selected
+                        </button>
+                    </div>
+                    <ul className="space-y-2">
+                        {
+                            shoppingList.map(
+                                item => (
+                                    <li 
+                                        key={item.id}
+                                    >   
+                                        <Card className="flex items-center justify-between">
+                                            {item.name}
+                                            &nbsp;
+                                            {   
+                                                item.quantity > 1 &&
+                                                `(${item.quantity}x)`
                                             }
-                                            <ButtonAdd />
-                                        </div>
-                                    </Card>
-                                </li>
+                                            <div className="flex gap-2">
+                                                {
+                                                    item.quantity > 1 && <ButtonSubtract />
+                                                }
+                                                <ButtonAdd />
+                                            </div>
+                                        </Card>
+                                    </li>
+                                )
+                            
                             )
-                        
-                        )
-                    }
-                </ul>
+                        }
+                    </ul>
+                </div>
             </main>
         </>
     )
