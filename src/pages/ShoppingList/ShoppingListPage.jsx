302cc222cbcfdd411e088668fa566d540623337d
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-06-26 14:08:14 -0500

    add basic styles and markup

--- src/pages/ShoppingList/ShoppingListPage.jsx
+++ src/pages/ShoppingList/ShoppingListPage.jsx
@@ -5,6 +5,8 @@ import AddItemForm from "./AddItemForm"
 import { useAtomValue } from "jotai"
 import { formDataAtom } from "../../store/store"
 import LinkNav from "../../components/Links/LinkNav"
+import Card from "../../components/Card"
+import Button from "../../components/Buttons/Button"
 
 export default function ShoppingListPage() {
     const formData = useAtomValue(formDataAtom)
@@ -21,6 +23,18 @@ export default function ShoppingListPage() {
                     formData ? <AddItemForm /> : <LinkNav to="recipes">Recipes</LinkNav>
                 }
             </main>
+
+            <dialog open>
+                <div className="bg-white/10 backdrop-blur fixed inset-0 flex flex-col justify-center gap-4 px-4">
+                    <Card className="text-white grid gap-4 px-2 pt-4 bg-black/50">
+                        <p className="text-center">Are you sure?</p>
+                        <div className="flex gap-2">
+                            <Button className="flex-grow bg-green-900">Yes</Button>
+                            <Button className="bg-red-900">No</Button>
+                        </div>
+                    </Card>
+                </div>
+            </dialog>
         </>
     )
 }
