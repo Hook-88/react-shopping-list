c8e3f13db504717a633a4b14adf8f78ac5071c1a
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-06-26 17:03:36 -0500

    improved confirm dialog

--- src/pages/ShoppingList/ShoppingListEl.jsx
+++ src/pages/ShoppingList/ShoppingListEl.jsx
@@ -1,14 +1,14 @@
 import ShoppingListHeader from "./ShoppingListHeader"
 import ShoppingListDefault from "./ShoppingListDefault"
 import ShoppingListFilterNotSelected from "./ShoppingListFilterNotSelected"
-import { useAtomValue } from "jotai"
-import { listFiltersAtom, shoppingListAtom } from "../../store/store"
+import { useAtomValue, useSetAtom } from "jotai"
+import { listFiltersAtom, shoppingListAtom, confirmDialogAtom } from "../../store/store"
 import List from "../../components/List/List"
 
 export default function ShoppingListEl() {
     const shoppingList = useAtomValue(shoppingListAtom)
     const filters = useAtomValue(listFiltersAtom)
-    
+
     return (
         <List listArr={shoppingList}>
             <ShoppingListHeader />
