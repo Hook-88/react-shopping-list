a5c337cecd72bec8bf1822e3166b6a13f6859e92
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-06-25 20:29:39 -0500

    refactor code

--- src/pages/ShoppingList/ShoppingListHeader.jsx
+++ src/pages/ShoppingList/ShoppingListHeader.jsx
@@ -3,11 +3,15 @@ import { shoppingListAtom } from "../../store/store"
 import { FaEyeSlash } from "react-icons/fa6"
 
 export default function ShoppingListHeader() {
+    const someChecked = useAtomValue(shoppingListAtom).some(item => item.selected === true)
     
     return (
         <div className="px-4 mb-1 flex">
             <small>(2/2)</small>
-            <button className="text-sm ml-auto flex items-center gap-1">
+            <button 
+                className="text-sm ml-auto flex items-center gap-1 disabled:text-white/50"
+                disabled={!someChecked}
+            >
                 <FaEyeSlash />
                 hide selected
             </button>
