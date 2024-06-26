d13be76a2c2bb319ce293f05e3bbc6d3ddb311c4
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-06-25 21:45:12 -0500

    improved menu

--- src/pages/ShoppingList/ShoppingListMenu.jsx
+++ src/pages/ShoppingList/ShoppingListMenu.jsx
@@ -2,13 +2,20 @@ import PageHeader from "../../components/PageHeader/PageHeader"
 import ShoppingListEl from "./ShoppingListEl"
 import Menu from "./../../components/Menu/Menu"
 import IconMore from "../../components/Icons/IconMore"
+import { useSetAtom } from "jotai"
+import { menuOpenAtom } from "../../store/store"
 
 export default function ShoppingListMenu() {
+    const setOpen = useSetAtom(menuOpenAtom)
+
+    function toggleOpen() {
+        setOpen(prev => !prev)
+    }
     
     return (
         <Menu className="col-span-2">
             <Menu.Button className="flex justify-end items-center pr-5"><IconMore /></Menu.Button>
-            <Menu.Dropdown>
+            <Menu.Dropdown onClick={toggleOpen}>
                 <Menu.Item>Add item</Menu.Item>
                 <Menu.Item>Delete selected</Menu.Item>
             </Menu.Dropdown>
