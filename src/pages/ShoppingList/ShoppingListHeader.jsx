3c0a65d24e418efc54041d46c24f0022e1f54c2b
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-06-25 21:01:51 -0500

    refactor code

--- src/pages/ShoppingList/ShoppingListHeader.jsx
+++ src/pages/ShoppingList/ShoppingListHeader.jsx
@@ -1,13 +1,7 @@
-import { useAtomValue, useAtom } from "jotai"
-import { listFiltersAtom, shoppingListAtom } from "../../store/store"
 import ButtonFilterShoppingList from "./ButtonFilterShoppingList"
 import ShoppingListProgress from "./ShoppingListProgress"
 
 export default function ShoppingListHeader() {
-    const shoppingList = useAtomValue(shoppingListAtom)
-    const shoppingListFilteredSelected = shoppingList.filter(item => item.selected === true)
-    const [filters, setFilters] = useAtom(listFiltersAtom)
-
     
     return (
         <div className="px-4 mb-1 flex">
