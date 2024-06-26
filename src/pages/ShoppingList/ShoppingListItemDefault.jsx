5c255be2f8191d0ef0a95a054eef45ffe8dfe3cf
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-06-25 21:07:22 -0500

    add logic to modify quantity

--- src/pages/ShoppingList/ShoppingListItemDefault.jsx
+++ src/pages/ShoppingList/ShoppingListItemDefault.jsx
@@ -1,8 +1,27 @@
 import Card from "../../components/Card"
 import ButtonAdd from "../../components/Buttons/ButtonAdd"
 import ButtonSubtract from "../../components/Buttons/ButtonSubtract"
+import { doc, getDoc, updateDoc } from "firebase/firestore"
+import { db } from "../../firebase/firebase"
 
 export default function ShoppingListItemDefault({item}) {
+    
+    function handleClickAdd(event) {
+        event.stopPropagation()
+        modifyQuantityInFirebase(1)
+    }
+
+    function handleClickSubtract(event) {
+        event.stopPropagation()
+        modifyQuantityInFirebase(-1)
+    }
+    
+    async function modifyQuantityInFirebase(num) {
+        const docRef = doc(db, "shoppingList", item.id)
+        const docSnap = await getDoc(docRef)
+
+        await updateDoc(docRef, {quantity: docSnap.data().quantity + num})
+    }
     
     return ( 
         <Card className="flex items-center justify-between">
@@ -14,9 +33,9 @@ export default function ShoppingListItemDefault({item}) {
             }
             <div className="flex gap-2">
                 {
-                    item.quantity > 1 && <ButtonSubtract />
+                    item.quantity > 1 && <ButtonSubtract onClick={handleClickSubtract}/>
                 }
-                <ButtonAdd />
+                <ButtonAdd onClick={handleClickAdd}/>
             </div>
         </Card>
     )
