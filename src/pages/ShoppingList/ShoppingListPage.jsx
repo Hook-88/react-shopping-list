c67f56dac4dc6a494b0ab5ddae98c001716d50c0
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-06-25 21:37:51 -0500

    add menu

--- src/pages/ShoppingList/ShoppingListPage.jsx
+++ src/pages/ShoppingList/ShoppingListPage.jsx
@@ -1,15 +1,21 @@
-import { useAtomValue } from "jotai"
-import { shoppingListAtom } from "../../store/store"
 import PageHeader from "../../components/PageHeader/PageHeader"
 import ShoppingListEl from "./ShoppingListEl"
+import Menu from "./../../components/Menu/Menu"
+import IconMore from "../../components/Icons/IconMore"
 
 export default function ShoppingListPage() {
-    const shoppingList = useAtomValue(shoppingListAtom)  
     
     return (
         <>
             <PageHeader>
                 <PageHeader.Title className="col-start-3 col-span-5">Shopping List</PageHeader.Title>
+                <Menu className="col-span-2">
+                    <Menu.Button className="flex justify-end items-center pr-5"><IconMore /></Menu.Button>
+                    <Menu.Dropdown>
+                        <Menu.Item>Add item</Menu.Item>
+                        <Menu.Item>Delete selected</Menu.Item>
+                    </Menu.Dropdown>
+                </Menu>
             </PageHeader>
             <main className="mt-12 px-4">
                 <ShoppingListEl />
--- src/store/store.js
+++ src/store/store.js
@@ -2,4 +2,5 @@ import { atom } from "jotai"
 
 export const shoppingListAtom = atom([])
 export const listFiltersAtom = atom([])
+export const menuOpenAtom = atom(false)
 
