2496b6cba9ae3ee192335f92afe990d9b993328e
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-06-28 13:37:32 -0500

    add onconfirmdialog when clicking list progress completed

--- src/pages/ShoppingList/AddItemToShoppingListEl.jsx
+++ src/pages/ShoppingList/AddItemToShoppingListEl.jsx
@@ -1,38 +1,8 @@
-import { useContext, useEffect, useState } from "react"
 import Card from "../../components/Card"
 import AddItemToShoppingListForm from "./AddItemToShoppingListForm"
-import { collection, onSnapshot } from "firebase/firestore"
-import { db } from "../../firebase/firebase"
-import { ShoppingListContext } from "./ShoppingListContextComponent"
-import getCapString from "./../../utility/getCapString"
 import ListInlinePopularItems from "./ListInlinePopularItems"
 
 export default function AddItemToShoppingListEl() {
-    // const {popularItems} = useContext(ShoppingListContext)
-    // const [ popularItems, setPopularItems ] = useState(null)
-    // const { shoppingList } = useContext(ShoppingListContext)
-
-    // useEffect(() => {
-    //     const collectionRef = collection(db, "shoppingList/history/items")
-    //     const unsub = onSnapshot(collectionRef, collectionSnapshot => {
-    //         const arr = 
-    //             collectionSnapshot.docs
-    //                 .map(doc => ({ ...doc.data(), id: doc.id}))
-    //                 .sort((a, b) => b.quantity - a.quantity)
-
-    //         const uniqueArr = filterArrayByNames(arr, shoppingList)
-
-    //         setPopularItems(uniqueArr.slice(0,5))
-    //     })
-
-    //     return unsub
-    // }, [shoppingList])
-
-    // function filterArrayByNames(sourceArray, referenceArray) {
-    //     return sourceArray.filter(sourceObj => 
-    //         !referenceArray.some(refObj => refObj.name === sourceObj.name)
-    //     )
-    // }
     
     return (
         <Card className="px-2">
--- src/pages/ShoppingList/ListShoppingListEl.jsx
+++ src/pages/ShoppingList/ListShoppingListEl.jsx
@@ -1,26 +1,34 @@
 import { useContext } from "react"
 import List from "../../components/List/List"
 import { ShoppingListContext } from "./ShoppingListContextComponent"
-import ListItemCardDefault from "./ListItemCardDefault"
-import ListItemCardSelected from "./ListItemCardSelected"
 import ListShoppingListDefault from "./ListShoppingListDefault"
 import ListShoppingListFilter from "./ListShoppingListFilter"
 import { FilterContext } from "../../Context/FilterContextComponent"
 import { FaEye, FaEyeSlash } from "react-icons/fa6"
+import { DialogConfirmContext } from "../../components/DialogConfirm/DialogConfirm"
 
 export default function ListShoppingListEl() {
     const { shoppingList, deleteSelectioninFirebase } = useContext(ShoppingListContext)
-    const {filters, setFilters, clearFilters} = useContext(FilterContext)
+    const { filters, setFilters, clearFilters } = useContext(FilterContext)
+    const { dialogObj, setDialogObj } = useContext(DialogConfirmContext)
 
     function toggleFilterSelected() {
         filters ? clearFilters() : setFilters('selected')
     }
+
+    function handleClickListProgress() {
+        setDialogObj({
+            question: "Delete checked items?",
+            confirmCallbackFn: () => deleteSelectioninFirebase()
+        })
+    }
+
     
     return (
         shoppingList.length > 0 ?
         <List listArr={shoppingList}>
             <List.Header className="flex items-center justify-between">
-                <List.Progress onClick={deleteSelectioninFirebase}/>
+                <List.Progress onClick={handleClickListProgress}/>
                 <button 
                     className="flex items-center pr-1.5 disabled:text-white/50"
                     onClick={toggleFilterSelected}
