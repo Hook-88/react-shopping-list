372875fc180f086513b6fdd64c28de8066ea8479
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-07-03 10:01:12 -0500

    Subject line (try to keep under 50 characters)
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- src/pages/RecipeEdit/RecipeEditPage.jsx
+++ src/pages/RecipeEdit/RecipeEditPage.jsx
@@ -4,55 +4,26 @@ import { useContext } from "react"
 import { RecipeContext } from "../Recipe/RecipeState"
 import Card from "../../components/Card"
 import List from "../../components/List/List"
-import { useNavigate, useParams } from "react-router-dom"
-import getCapString from "../../utility/getCapString"
-import Button from "../../components/Buttons/Button"
+import { useParams } from "react-router-dom"
 import { FormContext } from "../../Context/FormContextComponent"
 import AddIngredientEl from "./AddIngredientEl"
 import RecipeEditMenu from "./RecipeEditMenu"
-import EditIngredientEl from "./EditIngredientEl"
 import { DialogConfirmContext } from "../../components/DialogConfirm/DialogConfirm"
 import DialogConfirmEl from "../../components/DialogConfirm/DialogConfirmEl"
-import { deleteDoc, doc } from "firebase/firestore"
-import { db } from "../../firebase/firebase"
 import DeleteRecipeEl from "./DeleteRecipeEl"
+import ListIngredientsList from "./ListIngredientsList"
 
 export default function RecipeEditPage() {
-    const {recipeId} = useParams()
-    const { formData, handleChange, clearFormData, openForm } = useContext(FormContext)
-    const { dialogObj, setDialogObj } = useContext(DialogConfirmContext)
+    const { recipeId } = useParams()
+    const { formData } = useContext(FormContext)
+    const { dialogObj } = useContext(DialogConfirmContext)
     const {
         recipeObj, 
         updateRecipeObj,
-        setRecipeObj, 
-        clearRecipeObj, 
-        setIngredientsSelect
     
     } = useContext(RecipeContext)
-    
-    const navigate = useNavigate()
 
     updateRecipeObj(recipeId)
-
-    function selectIngredient(ingredientId) {
-        setRecipeObj(prevRecipeObj => (
-            {
-                ...prevRecipeObj,
-                ingredients: prevRecipeObj.ingredients
-                    .map(ingredient => 
-                        ingredient.id === ingredientId ? 
-                        { ...ingredient, selected: true } : 
-                        { ...ingredient, selected: false }
-                    )
-            }
-
-        ))
-    }
-
-    function handleClickIngredient(ingredientId) {
-        selectIngredient(ingredientId)
-        clearFormData()
-    }
     
     return (
         <>
@@ -80,25 +51,9 @@ export default function RecipeEditPage() {
                     <List.Header>
                         <small>Ingredients</small>
                     </List.Header>
-                    <List.List>
-                        {
-                            ingredients => ingredients.map(ingredient => (
-                                <li key={ingredient.id} onClick={() => handleClickIngredient(ingredient.id)}>
-                                    {
-                                        ingredient.selected ?
-                                        <EditIngredientEl ingredient={ingredient}/> :
-                                        <Card>
-                                            {getCapString(ingredient.name)}
-                                        </Card>
-                                    }
-                                </li>
-                            ))
-                        }
-                    </List.List>
+                    <ListIngredientsList />
                 </List>
-                {
-                    formData && <AddIngredientEl />
-                }
+                { formData && <AddIngredientEl /> }
                 <DeleteRecipeEl />
             </main>
         }
