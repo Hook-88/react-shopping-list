4c61de3a1d1c78a6e58fc4ffdf4ff80d4469513e
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-07-04 19:05:23 -0500

    refactor code

--- src/pages/ShoppingList/ShoppingListPage.jsx
+++ src/pages/ShoppingList/ShoppingListPage.jsx
@@ -1,5 +1,7 @@
 import { useAtomValue } from "jotai"
 import { shoppingListAtom } from "../../store/store"
+import getStringFirstCharCap from "../../utility/getStringFirstCharCap"
+import { FaMinus, FaPlus } from "react-icons/fa6"
 
 export default function ShoppingListPage() {
     const shoppingList = useAtomValue(shoppingListAtom)
@@ -9,13 +11,37 @@ export default function ShoppingListPage() {
             <header className="bg-white/10 py-2">
                 <h1 className="text-center text-lg font-bold">Shoping List</h1>
             </header>
-            <main>
+            <main className="p-4">
                 {
                     shoppingList &&
                     <ul>
                         {
                             shoppingList.map(item => (
-                                <li key={item.id}>{item.name}</li>
+                                <li 
+                                    key={item.id}
+                                    className="py-2 px-4 border border-transparent mb-3 rounded-md bg-white/10 flex items-center"
+                                >
+                                    {getStringFirstCharCap(item.name)}
+                                    {
+                                        item.quantity > 1 &&
+                                        <>
+                                            &nbsp;
+                                            ({item.quantity}x)
+                                        </>
+                                    }
+                                    <div className="flex gap-2 ml-auto">
+                                        {
+                                            item.quantity > 1 &&    
+                                            <button className="p-1 border rounded-md">
+                                                <FaMinus />
+                                            </button>
+                                        }
+                                        <button className="p-1 border rounded-md">
+                                            <FaPlus />
+                                        </button>
+
+                                    </div>
+                                </li>
                             ))
                         }
                     </ul>
---
+++ src/utility/getStringFirstCharCap.jsx
@@ -0,0 +1,4 @@
+export default function getStringFirstCharCap(str) {
+    
+    return str[0].toUpperCase() + str.slice(1).toLowerCase()
+}
