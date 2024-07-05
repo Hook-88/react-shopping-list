2fe5f4d1ffb751bd9681d08c624520151b725801
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-07-04 21:04:10 -0500

    add logic to modify quantity

--- src/pages/ShoppingList/ListItemDefault.jsx
+++ src/pages/ShoppingList/ListItemDefault.jsx
@@ -1,28 +1,49 @@
+import { doc, getDoc, updateDoc } from "firebase/firestore"
 import getStringFirstCharCap from "../../utility/getStringFirstCharCap"
 import { FaMinus, FaPlus } from "react-icons/fa6"
+import { db } from "../../firebase"
 
 export default function ListItemDefault({item}) {
+
+    async function modifyFirbaseItemQuantity(num) {
+        const docRef = doc(db, "shoppingList", item.id)
+        const docSnap = await getDoc(docRef)
+
+        await updateDoc(docRef, {quantity: docSnap.data().quantity + num})
+    }
+
+    function handleClickDecrement(event) {
+        event.stopPropagation()
+        modifyFirbaseItemQuantity(-1)
+    }
+
+    function handleClickIncrement(event) {
+        event.stopPropagation()
+        modifyFirbaseItemQuantity(1)
+    }
     
     return (
         <div 
-            className="py-2 px-4 border border-transparent mb-3 rounded-md bg-white/10 flex items-center"
+            className="py-2 px-4 border border-transparent mb-3 rounded-md bg-white/10 flex items-center gap-1"
         >
-            {getStringFirstCharCap(item.name)}
-            {
-                item.quantity > 1 &&
-                <>
-                    &nbsp;
-                    ({item.quantity}x)
-                </>
-            }
+            <p>{getStringFirstCharCap(item.name)}</p>
+            
+            { item.quantity > 1 && `(${item.quantity}x)` }
+
             <div className="flex gap-2 ml-auto">
                 {
                     item.quantity > 1 &&    
-                    <button className="p-1 border border-white/10 rounded-md bg-red-900">
+                    <button 
+                        className="p-1 border border-white/10 rounded-md bg-red-900"
+                        onClick={handleClickDecrement}
+                    >
                         <FaMinus />
                     </button>
                 }
-                <button className="p-1 border rounded-md border-white/10 bg-sky-900">
+                <button 
+                    className="p-1 border rounded-md border-white/10 bg-sky-900"
+                    onClick={handleClickIncrement}
+                >
                     <FaPlus />
                 </button>
 
--- src/pages/ShoppingList/ListShoppingListEl.jsx
+++ src/pages/ShoppingList/ListShoppingListEl.jsx
@@ -1,68 +1,20 @@
 import { useAtom, useAtomValue } from "jotai"
 import { listFiltersAtom, shoppingListAtom } from "../../store/store"
-import ListItemDefault from "./ListItemDefault"
-import ListItemSelected from "./ListItemSelected"
 import { doc, getDoc, updateDoc } from "firebase/firestore"
 import { db } from "../../firebase"
-import { FaEye, FaEyeSlash } from "react-icons/fa6"
 import ListDefault from "./ListDefault"
 import ListFilterNotSelected from "./ListFilterNotSelected"
+import ListHeader from "./ListHeader"
 
 export default function ListShoppingListEl() {
-    const shoppingList = useAtomValue(shoppingListAtom)
     const [filter, setFilter] = useAtom(listFiltersAtom)
-    const numOfCheckedItems = shoppingList.filter(item => item.selected === true).length
-
-    async function toggleFirebaseItemSelect(itemId) {
-        const docRef = doc(db, "shoppingList", itemId)
-        const docSnap = await getDoc(docRef)
-
-        await updateDoc(docRef, {selected: !docSnap.data().selected})
-    }
-
-    function toggleFilterSelected() {
-        if (filter) {
-            setFilter(null)
-            
-            return
-        }
-
-        setFilter("!selected")
-    }
     
     return (
         <>
-            <div className="flex items-center justify-between px-4 mb-1">
-                <small>
-                    {`(${numOfCheckedItems}/${shoppingList.length})`}
-                </small>
-
-                <button 
-                    className="flex items-center disabled:text-white/50" 
-                    onClick={toggleFilterSelected}
-                    disabled={numOfCheckedItems === 0}
-
-                >
-                    <small className="flex items-center gap-1">
-                        {
-                            filter ? 
-                            <>
-                                Show selected
-                                <FaEye />
-                            </> :
-                            <>
-                                Hide selected
-                                <FaEyeSlash />
-                            </>
-                        }
-                    </small>
-                </button>
-            </div>
-
+            <ListHeader />
             {
                 filter ? <ListFilterNotSelected /> : <ListDefault />
             }
-
         </>
 
     )
