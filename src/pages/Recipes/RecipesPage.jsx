18c7ef787fc10ab2320804fddd85b47e16f8466f
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-07-07 10:31:16 -0500

    add basic markup

--- src/pages/Recipes/RecipesPage.jsx
+++ src/pages/Recipes/RecipesPage.jsx
@@ -43,9 +43,9 @@ export default function RecipesPage() {
                     </Menu.Dropdown>
                 </Menu>
             </PageHeader>
-            {
-                recipes && (
-                    <PageMain>
+                <PageMain>
+                {
+                    recipes && (
                         <ul>
                             {
                                 recipes.map(recipe => (
@@ -57,9 +57,37 @@ export default function RecipesPage() {
                                 ))
                             }
                         </ul>
-                    </PageMain>
-                )
-            }
+                    )
+                }
+                <div className="bg-white/10 p-2 rounded-md">
+                    <form className="grid gap-2" 
+                        // onSubmit={handleSubmit(sendFormData)}
+                    >
+                        <input 
+                            type="text" 
+                            className="py-1 px-2 rounded-md bg-white/10"
+                            placeholder="Item..."
+                            // {...register("itemName")}
+                            required
+                            autoFocus
+                        />
+                        <div className="flex gap-2">
+                            <button 
+                                className="flex-grow py-1 bg-green-900 rounded-md border border-white/10"
+                            >
+                                Add
+                            </button>
+                            <button 
+                                className="px-2 bg-red-900 rounded-md border border-white/10" 
+                                type="button"
+                                // onClick={closeForm}
+                            >
+                                Close
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </PageMain>
         </>
     )
 }
