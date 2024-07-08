069324c2fe6b7d70326b9e7dce599e29e5ac21bf
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-07-08 18:57:04 -0500

    refactor code

--- src/pages/RecipeNameEdit/EditRecipeNameEl.jsx
+++ src/pages/RecipeNameEdit/EditRecipeNameEl.jsx
@@ -1,15 +1,10 @@
 import { useParams } from "react-router-dom"
-import PageHeader from "../../components/PageHeader/PageHeader"
-import useRecipeNameSnapshot from "../../hooks/useRecipeNameSnapshot"
 import getStringFirstCharCap from "../../utility/getStringFirstCharCap"
-import PageMain from "../../components/PageMain/PageMain"
 import { useForm } from "react-hook-form"
 import setFirebaseRecipeName from "../../utility/firestoreFn/setFirebaseRecipeName"
 
 export default function EditRecipeNameEl({nameValue}) {
     const params = useParams()
-    // const recipeName = useRecipeNameSnapshot(params.recipeId)
-    
     const {register, handleSubmit, reset} = useForm({
         defaultValues: {
             recipeName: getStringFirstCharCap(nameValue)
@@ -21,13 +16,15 @@ export default function EditRecipeNameEl({nameValue}) {
     }
     
     return (
-        <div className="bg-white/10 p-2 rounded-md">
+        <div 
+            className="bg-white/10 p-2 rounded-md"
+        >
             <form className="grid gap-2" 
                 onSubmit={handleSubmit(onSubmit)}
             >
                 <input 
                     type="text" 
-                    className="py-1 px-2 rounded-md bg-white/10"
+                    className="py-1 px-2 rounded-md bg-white/10 text-center"
                     placeholder="Recipe name..."
                     {...register("recipeName")}
                     required
@@ -38,13 +35,6 @@ export default function EditRecipeNameEl({nameValue}) {
                         className="flex-grow py-1 bg-green-900 rounded-md border border-white/10"
                     >
                         Save
-                    </button>
-                    <button 
-                        className="px-2 bg-red-900 rounded-md border border-white/10" 
-                        type="button"
-                        // onClick={handleClickClose}
-                    >
-                        Cancel
                     </button>
                 </div>
             </form>
--- src/pages/RecipeNameEdit/RecipeNameEditPage.jsx
+++ src/pages/RecipeNameEdit/RecipeNameEditPage.jsx
@@ -4,14 +4,23 @@ import useRecipeNameSnapshot from "../../hooks/useRecipeNameSnapshot"
 import getStringFirstCharCap from "../../utility/getStringFirstCharCap"
 import PageMain from "../../components/PageMain/PageMain"
 import EditRecipeNameEl from "./EditRecipeNameEl"
+import { useAtom } from "jotai"
+import { pageFormsOpenAtom } from "../../store/store"
+import NavLinkBack from "../../components/Links/NavLinkBack"
 
 export default function RecipeNameEditPage() {
     const params = useParams()
     const recipeName = useRecipeNameSnapshot(params.recipeId)
+    const [formOn, setFormOn] = useAtom(pageFormsOpenAtom)
+
+    function toggleFormOn() {
+        setFormOn(prev => !prev)
+    }
     
     return (
         <>
             <PageHeader>
+                <NavLinkBack />
                 <PageHeader.Title>
                     {recipeName?.name ? `${getStringFirstCharCap(recipeName.name)} (edit)` : "Loading..."}
                 </PageHeader.Title>
@@ -20,14 +29,7 @@ export default function RecipeNameEditPage() {
             {
                 recipeName && (
                     <PageMain>
-                            <div 
-                                className="py-2 px-4 border border-transparent rounded-md bg-white/10 flex items-center gap-1"
-                            >
-                                <p>{getStringFirstCharCap(recipeName.name)}</p>
-
-                            </div>
-                            <EditRecipeNameEl nameValue={recipeName.name}/>
-
+                        <EditRecipeNameEl nameValue={recipeName.name}/>
                     </PageMain>
                 )
             }
